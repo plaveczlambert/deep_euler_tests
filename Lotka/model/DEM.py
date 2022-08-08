00c988550353d7b1c94b577594ee272c9dfaf699
Lambert Plavecz <plaveczlambert@gmail.com>
2022-08-08 11:54:31 +0200

    Updated DEM implementation

--- Lotka/model/DEM.py
+++ Lotka/model/DEM.py
@@ -10,17 +10,20 @@ import torch.jit
 #torch.set_default_dtype(torch.float64)
 
 class DeepEuler(OdeSolver):
-
-    def __init__(self, fun, t0, y0, t_bound, h, traced_model_path, scaler_path="", absolute_time=False, **extraneous):
+    MODE_GENERAL = 2
+    MODE_AUTONOMOUS = 0
+    MODE_ABSOLUTE_TIMES = 1
+    
+    def __init__(self, fun, t0, y0, t_bound, h, traced_model_path, scaler_path="", mode=MODE_GENERAL, **extraneous):
         warn_extraneous(extraneous)
         super().__init__(fun, t0, y0, t_bound, vectorized=False, support_complex=False)
         
         self.h = h
-        self.abs_time = absolute_time
+        self.mode = mode
 
         self.model = torch.jit.load(traced_model_path)
         self.model.eval()
-        if not absolute_time and scaler_path:
+        if scaler_path:
             f = open(scaler_path,'r')
             self.out_scaler = StandardScaler(f)
             self.in_scaler = StandardScaler(f)
@@ -33,17 +36,21 @@ class DeepEuler(OdeSolver):
         y = self.y
         h = self.h
         
-        if self.abs_time:
+        dydt = self.fun(t, y)
+        
+        if self.mode==DeepEuler.MODE_AUTONOMOUS:
+            y_in = np.concatenate(([h], y))
+        elif self.mode==DeepEuler.MODE_ABSOLUTE_TIMES:
             y_in = np.concatenate(([t+h], [t], y))
         else:
-            y_in = np.concatenate(([h], y))
+            y_in = np.concatenate(([t], [h], y))
             
         if self.out_scaler:
             model_out = self.out_scaler.inverse_transform(self.model(torch.tensor(self.in_scaler.transform(y_in))).detach().numpy())
         else:
             model_out = self.model(torch.tensor(y_in)).detach().numpy()
         
-        y_new = y + h * self.fun(t, y) + h * h * model_out
+        y_new = y + h * dydt + h * h * model_out
         t_new = t + h
         
         self.y_old = y
