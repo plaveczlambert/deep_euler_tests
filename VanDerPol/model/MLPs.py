fa1bc30c322798e9e57a9ca456194e482d5a2bf2
Lambert Plavecz <plaveczlambert@gmail.com>
2022-08-19 17:37:49 +0200

    Add another tuned network

--- VanDerPol/model/MLPs.py
+++ VanDerPol/model/MLPs.py
@@ -204,3 +204,64 @@ class OptimizedMLP(nn.Module):
         x   = self.l_out(x)
         return x    
 
+class VdPPhaseSpaceOptimizedMLP(nn.Module):
+    def __init__(self, num_in_features:int, num_out_features:int):
+        super(OptimizedMLP, self).__init__()
+        
+        self.act    = nn.ELU()
+        
+        self.l_in   = nn.Linear(
+            in_features = num_in_features,
+            out_features= 23
+            )
+        self.l1   = nn.Linear(
+            in_features = 23,
+            out_features= 140
+            )
+        self.l2   = nn.Linear(
+            in_features = 140,
+            out_features= 88
+            )
+        self.l3   = nn.Linear(
+            in_features = 88,
+            out_features= 59
+            )
+        self.l4   = nn.Linear(
+            in_features = 59,
+            out_features= 167
+            )
+        self.l5   = nn.Linear(
+            in_features = 167,
+            out_features= 106
+            )
+        self.l_out   = nn.Linear(
+            in_features = 106,
+            out_features= num_out_features
+            )
+        #weight init
+        torch.nn.init.xavier_normal_(self.l_in.weight)
+        torch.nn.init.zeros_(self.l_in.bias)
+        torch.nn.init.xavier_normal_(self.l1.weight)
+        torch.nn.init.zeros_(self.l1.bias)
+        torch.nn.init.xavier_normal_(self.l2.weight)
+        torch.nn.init.zeros_(self.l2.bias)
+        torch.nn.init.xavier_normal_(self.l3.weight)
+        torch.nn.init.zeros_(self.l3.bias)
+        torch.nn.init.xavier_normal_(self.l4.weight)
+        torch.nn.init.zeros_(self.l4.bias)
+        torch.nn.init.xavier_normal_(self.l5.weight)
+        torch.nn.init.zeros_(self.l5.bias)
+        torch.nn.init.xavier_normal_(self.l_out.weight)
+        torch.nn.init.zeros_(self.l_out.bias)
+        
+       
+
+    def forward(self, x):
+        x   = self.act(self.l_in(x))
+        x   = self.act(self.l1(x))
+        x   = self.act(self.l2(x))
+        x   = self.act(self.l3(x))
+        x   = self.act(self.l4(x))
+        x   = self.act(self.l5(x))
+        x   = self.l_out(x)
+        return x  
